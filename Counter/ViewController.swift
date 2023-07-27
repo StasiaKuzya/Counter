87d1137743dd815dcfa219dd4811a53c32fbcec8
Anastasia Kuzina <kuzina-sta@mail.ru>
2023-07-27 20:18:38 +0300

    Созданы кнопка и лейбл При старте приложение у лейбла показан текст "0"; При нажатии на кнопку значение счётчика увеличивается на 1 и отображается в тексте лейбла.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countButton: UIButton!
+    
+    @IBOutlet weak var countResultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countResultLabel.text = "Значение счётчика: 0"
     }
+    private var countResult: Int = 1
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        countResultLabel.text = "Значение счётчика: \(countResult)"
+        return countResult += 1
+    }
+    
 }
 
